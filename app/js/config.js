1835ecd1c3bed5a9f28f952b8b828165ca50ac3d
torub <toru99@gmail.com>
2013-06-16 13:01:20 +0900

    2nd commit

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function() {
+  var config = {};
+  config.apiKey = 'AIzaSyDM5YIWoB3y_6XW2gt0hIra9dM5oschSX4';
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientId = '281960208185-7831673lbjutkrls47ivv0nlgioea7l6.apps.googleusercontent.com';
+  return config;
+});
